b6ac1d70e7b4672d839e6a2b94d88454e41cd395
shoiti <shoiti.tokunaga@gmail.com>
2021-03-22 13:11:15 -0300

    React - Criação de elemento com class

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Importar o css para que o React encontre a class.
+import './index.css';
+
 // React.createElement( elemento, propriedades, filho) - Criação de um elemento no html.
 const textHello = React.createElement('h1', null, 'Oi mundo!');
 
+// Criação de um elemento com uma class chamada .text-color-blue
+const textHelloWithClass = React.createElement(
+    'h1',
+    { className: 'text-color-blue' },
+    'Olá mundo!'
+)
+
 // ReactDOM.render( componente, container ) - Renderiza o componente React no html
 const container = document.getElementById('app');
-ReactDOM.render(textHello, container);
+ReactDOM.render(textHelloWithClass, container);
