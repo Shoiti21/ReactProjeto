e22f25e45792a04aa2252a45b02900bc0d9c59a5
shoiti <shoiti.tokunaga@gmail.com>
2021-03-22 13:01:59 -0300

    React - Criação e Renderização de um componente React

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// React.createElement( elemento, propriedades, filho) - Criação de um elemento no html.
+const textHello = React.createElement('h1', null, 'Oi mundo!');
+
+// ReactDOM.render( componente, container ) - Renderiza o componente React no html
+const container = document.getElementById('app');
+ReactDOM.render(textHello, container);
